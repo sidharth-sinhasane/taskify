7b9b89bd90ed0903fbfa564412e32b5c9881eb3f
sidharth_sinhasane <sidharth.sinhasane@gmail.com>
2024-10-10 10:18:12 +0530

    database schemas are created + models are exported

--- server/db/index.js
+++ server/db/index.js
@@ -12,4 +12,38 @@ const connectToDatabase = async function (){
     }
 }
 
-connectToDatabase()
+
+const userSchema= new mongoose.Schema({
+    userName : {
+        type:String,
+        required :true
+    },
+    password :{
+        type:String,
+        required :true
+    }
+})
+
+const todoSchema = new mongoose.Schema({
+    title: String,
+    userId : mongoose.Schema.ObjectId,
+    completed : Boolean
+})
+
+
+const User= mongoose.model('User',userSchema)
+const Todo=mongoose.model('Todo',todoSchema)
+
+// const mytodo=new Todo ({
+//     title : "pack tifin",
+//     completed : false
+// })
+
+// const somtodos= Todo.find()
+// console.log(somtodos)
+
+module.exports={
+    connectToDatabase,
+    User,
+    Todo
+}
